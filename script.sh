a6f2fd2545a2af0864503ec96dc7c3e914c48654
AkshayGets <40195161+AkshayGets@users.noreply.github.com>
2020-10-21 12:18:52 +0530

    c1

--- script.sh
+++ script.sh
@@ -2,7 +2,6 @@ TIMESTAMP=$(date +"%Y-%m-%d")
 deletionDate=$TIMESTAMP
 count=2
 creationDate=$( echo `date -d "${targetDate} -${count} days" +"%Y-%m-%d"`)
-echo "Auto Generate Log files"
 find /home/ec2-user/ -type f -name '*.wav' -mtime +2 | xargs -0 -n 1 > deleted-files-$TIMESTAMP.log
 echo "Log Generated Successfully"
 sed "s/$/\t${creationDate}/" -i /home/ec2-user/deleted-files-$TIMESTAMP.log
