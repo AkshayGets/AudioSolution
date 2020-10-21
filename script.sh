84a23b31d1e2bcf1f3f80fa350275f3b1f163f75
AkshayGets <40195161+AkshayGets@users.noreply.github.com>
2020-10-21 12:17:27 +0530

    updated +2

--- script.sh
+++ script.sh
@@ -3,9 +3,9 @@ deletionDate=$TIMESTAMP
 count=2
 creationDate=$( echo `date -d "${targetDate} -${count} days" +"%Y-%m-%d"`)
 echo "Auto Generate Log files"
-find /home/ec2-user/ -type f -name '*.wav' -mtime -1 | xargs -0 -n 1 > deleted-files-$TIMESTAMP.log
+find /home/ec2-user/ -type f -name '*.wav' -mtime +2 | xargs -0 -n 1 > deleted-files-$TIMESTAMP.log
 echo "Log Generated Successfully"
 sed "s/$/\t${creationDate}/" -i /home/ec2-user/deleted-files-$TIMESTAMP.log
 sed "s/$/\t${deletionDate}/" -i /home/ec2-user/deleted-files-$TIMESTAMP.log
-find /home/ec2-user/ -type f -name '*.wav' -mtime -1 -exec rm {} \;
+find /home/ec2-user/ -type f -name '*.wav' -mtime +2 -exec rm {} \;
 dd if=/dev/null of=deleted-files-$TIMESTAMP.log bs=1 seek=$(echo $(stat --format=%s deleted-files-$TIMESTAMP.log ) - $( tail -n1 deleted-files-$TIMESTAMP.log | wc -c) | bc )
