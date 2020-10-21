80bab467e9936986ecdd8d1ef8a9bc74b272357a
AkshayGets <40195161+AkshayGets@users.noreply.github.com>
2020-10-21 20:03:30 +0530

    update variable

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 TIMESTAMP=$(date +"%Y-%m-%d")
 deletionDate=$TIMESTAMP
 count=2
-creationDate=$( echo `date -d "${targetDate} -${count} days" +"%Y-%m-%d"`)
+creationDate=$( echo `date -d "${TIMESTAMP} -${count} days" +"%Y-%m-%d"`)
 find /home/ec2-user/ -type f -name '*.wav' -mtime +2 | xargs -0 -n 1 > deleted-files-$TIMESTAMP.log
 echo "Log Generated Successfully"
 sed "s/$/\t${creationDate}/" -i /home/ec2-user/deleted-files-$TIMESTAMP.log
